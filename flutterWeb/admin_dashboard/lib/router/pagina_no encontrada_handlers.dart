d04434832583ac02997198d78d9c2b5013af8865
Esteban Alvarez <juanes.x777@gmail.com>
2021-08-10 23:04:26 -0500

    codigo seccion 11 flutter web

--- flutterWeb/admin_dashboard/lib/router/pagina_no encontrada_handlers.dart
+++
@@ -1,9 +0,0 @@
-import 'package:fluro/fluro.dart';
-
-import 'package:admin_dashboard/ui/views/no_page_found.dart';
-
-class NoPageFoundHandlers {
-  static Handler noPageFound = Handler(handlerFunc: (context, params) {
-    return NoPageFoundView();
-  });
-}
